feb43ba133c4d6a283fb9766903152dd35b13a5f
Ivy Patterson <105239600+ivy-p@users.noreply.github.com>
2022-09-06 16:25:39 -0600

    Random image generator working

--- js/script.js
+++ js/script.js
@@ -4,13 +4,32 @@ const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
 const getImage = async function () {
-    const res = await fetch("https://picsum.photos/v2/list?limit=100.");
+    const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
 //remember to put API addresses in quotes, or the double slash will turn your link into a comment.
-    console.log(images);
+    selectRandomImage(images);
 };
 
-getImage();
+
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    displayImage(randomImage);
+};
+
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+button.addEventListener("click", function () {
+    getImage();
+});
 
 
-
+// Click the button to see a random image. You may need to allow a few seconds for the images to appear. Press it again to reveal another!
+// In the command line, add and commit your changes. Push the changes up to GitHub.
+// Copy the link to your repo and submit it below.💥 BOOM💥 You did it, Skillcrusher!
