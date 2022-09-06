ff69e2d2f2d48bd41165a99a59567a2cd217f879
Ivy Patterson <105239600+ivy-p@users.noreply.github.com>
2022-09-06 16:00:23 -0600

    Initial commit

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const getImage = async function () {
+    const res = await fetch("https://picsum.photos/v2/list?limit=100.");
+    const images = await res.json();
+//remember to put API addresses in quotes, or the double slash will turn your link into a comment.
+    console.log(images);
+};
+
+getImage();
+
+
+
